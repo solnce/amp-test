bb42c56d0a971d7af9b7d5013ed1af40b1557b8d
solnce <solnce.jpn@gmail.com>
2021-05-20 21:39:16 +0900

    add test

---
+++ components/pats.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Parts = () => (
+  <div>
+    <h1>h1</h1>
+    <h2>h2</h2>
+    <h3>h3</h3>
+    <h4>h4</h4>
+    <div>title</div>
+    <amp-img
+      src="https://images.unsplash.com/photo-1533225737818-2a9101586b15?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1778&q=80"
+      alt='hoge'
+      height={100}
+      width={100}
+      layout='responsive'
+    />
+  </div>
+);
+
+export default Parts;
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,38 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Parts from "../../components/pats";
+import { renderToString } from 'react-dom/server';
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+export default async (req, res) => {
+  const response = {
+    items: [
+      {
+        body: renderToString(<Parts />),
+        title: 'タイトル１',
+        image: 'https://images.unsplash.com/photo-1533225737818-2a9101586b15?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1778&q=80',
+      },
+      {
+        body: renderToString(<Parts />),
+        title: 'タイトル２',
+        image: 'https://images.unsplash.com/photo-1533225737818-2a9101586b15?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1778&q=80',
+      },
+      {
+        body: renderToString(<Parts />),
+        title: 'タイトル３',
+        image: 'https://images.unsplash.com/photo-1533225737818-2a9101586b15?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1778&q=80',
+      },
+    ]
+  };
+
+  console.log(`response`, response)
+
+  res.statusCode = 200;
+  res.setHeader(
+    "AMP-Access-Control-Allow-Source-Origin",
+    "https://www.travelbook.co.jp"
+  );
+  res.setHeader(
+    "Access-Control-Expose-Headers",
+    "AMP-Access-Control-Allow-Source-Origin"
+  );
+
+  return res.json(response);
+};
