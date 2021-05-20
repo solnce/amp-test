2d25085c6322305370a2bf84a7087775c1e58567
solnce <solnce.jpn@gmail.com>
2021-05-21 02:12:24 +0900

    merge

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,59 @@ import React from "react";
 export const config = { amp: true };
 
 const Index = () => (
-  <div
-    dangerouslySetInnerHTML={{
-      __html: `
+  <div>
+    <div>
+      <select on="change:myTabs.toggle(index=event.value, value=true)">
+        <option value="0">Select tab 1</option>
+        <option value="1">Select tab 2</option>
+      </select>
+
+      <amp-selector
+        id="myTabs"
+        class="tabs-with-flex"
+        role="tablist"
+        keyboard-select-mode="focus"
+      >
+        <div
+          id="sample2-tabpanel1"
+          role="tabpanel"
+          aria-labelledby="sample2-tab1"
+          option="0"
+          selected
+        >
+          Tab one content...{" "}
+        </div>
+        <div
+          id="sample2-tabpanel2"
+          role="tabpanel"
+          aria-labelledby="sample2-tab2"
+          option="1"
+        >
+          <div
+            dangerouslySetInnerHTML={{
+              __html: `
     <amp-list width="auto" height="1000" layout="fixed-height" src="/api/hello">
       <template type="amp-mustache">
         {{{body}}}
       </template>
     </amp-list>
-
     `,
-    }}
-  />
+            }}
+          />
+        </div>
+        <style jsx>
+          {`
+            [role="tabpanel"] {
+              display: none;
+            }
+            [role="tabpanel"][selected] {
+              display: block;
+            }
+          `}
+        </style>
+      </amp-selector>
+    </div>
+  </div>
 );
 
 export default Index;
